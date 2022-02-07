9af2b05aa6a03e543531bd028373da9c7c6bfa79
Stijn <99053054@mydavinci.nl>
2022-02-07 14:31:31 +0100

    Added const for buttons

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let initButtons = (buttons) => {
     for(i=0; i != buttons.length; i++){
-        console.log("kanker")
         btns = document.createElement('button');
-        btns.innerText = 'test'
+        btns.innerText = buttons[i].title
         document.getElementById('button-wrapper').appendChild(btns);
     }
 }
