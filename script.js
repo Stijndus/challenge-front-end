4d8ecbb7e7deb64a2d9ed36e101692e86468806c
Stijndus <stijn@ardusit.nl>
2022-02-07 16:50:02 +0100

    Added calc button color function

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let initButtons = (buttons) => {
     document.getElementById('start-button').remove()
-    for(i=0; i != buttons.length; i++){
-        btns = document.createElement('button');
-        btns.innerText = buttons[i].title
-        document.getElementById('button-wrapper').appendChild(btns);
+    for(i=0; i <= buttons.length; i++){
+        let btn = document.createElement('button');
+        btn.innerText = buttons[i].title;
+        btn.className = 'w3-button';
+        calcBtnColor(buttons[i].title, btn);
+        document.getElementById('button-wrapper').appendChild(btn);
+    }
+}
+
+let calcBtnColor = (val, btn) => {
+    switch(val){
+        case 'Eens': 
+            btn.className += ' w3-green';
+            break;
+        case 'Oneens': 
+            btn.className += ' w3-red';
+            break;
+        default:
+            btn.className += ' w3-grey';
     }
 }
 
