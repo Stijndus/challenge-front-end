2b171208a33ef08417c2beab1bbea98a4e4f565d
Stijndus <stijn@ardusit.nl>
2022-02-08 13:21:17 +0100

    Prettifying

--- data.js
+++ data.js
@@ -1,6 +1,10 @@
 const buttons = [{
     title: 'Eens',
     value: 0,
-},{title: 'Geen van beide',
-value: 0,},{title: 'Oneens',
-value: 0,}]
+}, {
+    title: 'Geen van beide',
+    value: 0,
+}, {
+    title: 'Oneens',
+    value: 0,
+}]
